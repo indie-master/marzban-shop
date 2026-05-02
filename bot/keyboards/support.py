37a85cf763e89205f26b88ae3a8a3b6e2c9def8d
indie-master <indiemaster98@gmail.com>
2026-05-02 03:03:41 +0300

    fix: wire new UI env variables into docker-compose bot service

--- bot/keyboards/support.py
+++ bot/keyboards/support.py
@@ -7,9 +7,9 @@ import glv
 
 def get_support_keyboard() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
-    support_link = glv.config.get('SUPPORT_LINK')
+    support_link = glv.config.get('SUPPORT_URL')
     if support_link:
-        builder.row(InlineKeyboardButton(text=_("✍️ Write to support"), url=support_link))
+        builder.row(InlineKeyboardButton(text=_("✍️ Написать в поддержку"), url=support_link))
 
     builder.row(
         InlineKeyboardButton(
